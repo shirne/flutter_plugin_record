1837251fae6d849e63afed2945d586fbac8eb353
shirne <shirne@126.com>
2023-04-14 10:14:52 +0800

    export

--- lib/flutter_plugin_record.dart
+++ lib/flutter_plugin_record.dart
@@ -1,5 +1,7 @@
 export 'src/flutter_plugin_record.dart';
+export 'src/const/play_state.dart';
 export 'src/const/record_state.dart';
+export 'src/const/response.dart';
 export 'src/utils/common_toast.dart';
 export 'src/widgets/custom_overlay.dart';
 export 'src/widgets/voice_widget.dart';
